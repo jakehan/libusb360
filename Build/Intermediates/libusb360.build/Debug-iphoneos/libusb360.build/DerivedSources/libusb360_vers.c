f13730a4c9847365630aed97c72a58659d40641b
jakehan <jake_han@163.com>
2016-11-03 13:24:42 +0800

    Initial Commit

---
+++ Build/Intermediates/libusb360.build/Debug-iphoneos/libusb360.build/DerivedSources/libusb360_vers.c
@@ -0,0 +1,5 @@
+ extern const unsigned char libusb360VersionString[];
+ extern const double libusb360VersionNumber;
+
+ const unsigned char libusb360VersionString[] __attribute__ ((used)) = "@(#)PROGRAM:libusb360  PROJECT:libusb360-1" "\n";
+ const double libusb360VersionNumber __attribute__ ((used)) = (double)1.;
